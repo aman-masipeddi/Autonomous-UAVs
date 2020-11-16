65e47a2752828b3aea793b474105a211925a4c15
Aman Masipeddi <aman.masipeddi@gmail.com>
2020-11-16 11:27:12 -0600

    First push

---
+++ app/src/main/java/com/dji/cc01/MApplication.java
@@ -0,0 +1,23 @@
+package com.dji.cc01;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
+
+import com.secneo.sdk.Helper;
+
+public class MApplication extends Application {
+
+    private static Application app = null;
+
+    @Override
+    protected void attachBaseContext(Context paramContext) {
+        super.attachBaseContext(paramContext);
+        MultiDex.install(this);
+        com.secneo.sdk.Helper.install(this);
+        app = this;
+
+    }
+
+}
